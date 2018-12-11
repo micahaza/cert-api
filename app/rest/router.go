56bf39a3b1651541e7256b068848f6fecabaf373
Pető Zoltán <bajgli@gmail.com>
2018-12-11 22:16:30 +0100

    updated router

--- app/rest/router.go
+++ app/rest/router.go
@@ -3,14 +3,13 @@ package rest
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/micahaza/cert-api/app/database"
 )
 
 // Server ...
 type Server struct {
 	db     *database.CertsDB
-	router *mux.Router
+	router *http.ServeMux
 	// email  EmailSender
 }
 
@@ -18,13 +17,13 @@ type Server struct {
 func NewServer() *Server {
 	return &Server{
 		db:     database.NewCertsDB(),
-		router: mux.NewRouter().StrictSlash(true),
+		router: http.NewServeMux(),
 	}
 }
 
 func (s *Server) getAllCertificates() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// use thing
+
 	}
 }
 
@@ -66,11 +65,11 @@ func (s *Server) acceptCertificateTransfer() http.HandlerFunc {
 
 // Routes ...
 func (s *Server) Routes() {
-	s.router.HandleFunc("/certificates/", s.getAllCertificates()).Methods("GET")
-	s.router.HandleFunc("/certificates/", s.createCertificate()).Methods("POST")
-	s.router.HandleFunc("/users/{id}/certificates", s.getUserCertificates()).Methods("GET")
-	s.router.HandleFunc("/certificates/{id}", s.deleteCertificate()).Methods("DELETE")
-	s.router.HandleFunc("/certificates/{id}", s.updateCertificate()).Methods("PUT")
-	s.router.HandleFunc("/certificates/", s.transferCertificate()).Methods("POST")
-	s.router.HandleFunc("/certificates/", s.acceptCertificateTransfer()).Methods("PUT")
+	s.router.HandleFunc("/certificates/", s.getAllCertificates())            //.Methods("GET")
+	s.router.HandleFunc("/certificates/", s.createCertificate())             // .Methods("POST")
+	s.router.HandleFunc("/users/{id}/certificates", s.getUserCertificates()) //.Methods("GET")
+	s.router.HandleFunc("/certificates/{id}", s.deleteCertificate())         //.Methods("DELETE")
+	s.router.HandleFunc("/certificates/{id}", s.updateCertificate())         //.Methods("PUT")
+	s.router.HandleFunc("/certificates/", s.transferCertificate())           //.Methods("POST")
+	s.router.HandleFunc("/certificates/", s.acceptCertificateTransfer())     //.Methods("PUT")
 }
