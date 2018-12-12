09c9a259cb596eda944e69828b04731849c135e8
Pető Zoltán <bajgli@gmail.com>
2018-12-12 05:03:52 +0100

    router update

--- app/rest/router.go
+++ app/rest/router.go
@@ -37,6 +37,20 @@ func (s *Server) handleCertificate() http.HandlerFunc {
 	}
 }
 
+func (s *Server) handleOneCertificate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "PUT": // update certificate
+			allCerts, _ := s.db.AllCerts()
+			json.NewEncoder(w).Encode(allCerts)
+		case "DELETE": // delete certificate
+			fmt.Println("POST ...")
+		default:
+			fmt.Println("Method not allowed")
+		}
+	}
+}
+
 func (s *Server) createCertificate() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// use thing
@@ -44,12 +58,6 @@ func (s *Server) createCertificate() http.HandlerFunc {
 }
 
 func (s *Server) getUserCertificates() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// use thing
-	}
-}
-
-func (s *Server) deleteCertificate() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// use thing
 	}
@@ -75,12 +83,10 @@ func (s *Server) acceptCertificateTransfer() http.HandlerFunc {
 
 // Routes ...
 func (s *Server) Routes() {
-	s.router.HandleFunc("/certificates/", s.handleCertificate()) // GET -> list all, POST -> create new
-	// s.router.HandleFunc("/certificates/", s.createCertificate())             // .Methods("POST")
-
+	s.router.HandleFunc("/certificates/", s.handleCertificate())             // GET -> list all, POST -> create new
 	s.router.HandleFunc("/users/{id}/certificates", s.getUserCertificates()) //.Methods("GET")
+	s.router.HandleFunc("/certificates/{id}", s.handleOneCertificate())      //.Methods("PUT", "DELETE")
 	// s.router.HandleFunc("/certificates/{id}", s.deleteCertificate())         //.Methods("DELETE")
-	s.router.HandleFunc("/certificates/{id}", s.updateCertificate()) //.Methods("PUT")
 	// s.router.HandleFunc("/certificates/", s.transferCertificate())       //.Methods("POST")
 	// s.router.HandleFunc("/certificates/", s.acceptCertificateTransfer()) //.Methods("PUT")
 }
